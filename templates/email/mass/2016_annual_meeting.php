130473651b2a28bde8f629d7874b28891e745a29
Kia Kroas <kiafaldorius@gmail.com>
2016-09-28 12:20:30 -0700

    new 2016 annual meeting email

---
+++ templates/email/mass/2016_annual_meeting.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta content='width=device-width' name='viewport'>
+<meta content='text/html; charset=UTF-8' http-equiv='Content-Type'>
+<title>MagLaboratory Email</title>
+</head>
+<body bgcolor='#f6f6f6' style='height:100%;width:100%;'>
+<table style='width:100%;padding:20px;'>
+<tr>
+<td></td>
+<td bgcolor='#fafafa' class='container' style='clear: both !important; display: block !important; max-width: 600px !important; margin: 0 auto; padding: 20px; border: 1px solid #efefef;'>
+<div style='display:block;max-width:600px;margin: 0 auto;'>
+<table bgcolor='#fafafa' style='width:100%;'>
+<tr>
+<td>
+<img alt='MAG Laboratory Logo' src='http://www.maglaboratory.org/images/email-logo.png'>
+</td>
+</tr>
+<tr>
+<td>
+<h1>Annual Meeting 10/2/16 Reminder, Agenda, and Announcement</h1>
+<br>
+<p>We will be having our annual meeting again this year on 10/2/16 at MAGLab from 3pm to 7pm proceeded by a potluck.  If you missed our previous announcements about it, we hope you get this one and can attend!</p>
+<br>
+<p>
+During this annual meeting we will be having some pretty important re-organizing
+and discuss about MAGLab's structure, and all are invited to attend!
+Participation is key to the success of MAGLab and we need everyone to step out of the weeds to help express their interest in the community and decide our direction for the next year.
+This means decisions on who y'all think could lead MAGLab, as well as coming up with new fun ways to use MAGLab and try new things.
+</p>
+<br>
+<p>
+We've put together an annual meeting agenda that can be found here:
+<a href="https://docs.google.com/document/d/1xSwzIwOM6SsGB1pbDKu7CvxTHqTFA6gUotNH2iyeup4/edit?usp=sharing">Annual Meeting Agenda 2016 MAGLab</a>
+</p>
+<br>
+<p>
+There will be time for unscheduled issues to be discussed for those that show.
+</p>
+<br>
+<p>
+Brief summary:
+<ul>
+<li>-officer and board positions (all positions are open, and some incumbents are either running for different positions or not running this year or have moved away)</li>
+<li>-discussion of direction of maglab</li>
+<li>-space progress and status reports</li>
+<li>-bylaws discussion</li>
+<li>-curator position</li>
+</ul>
+</p>
+<br>
+<p>Additionally, I am officially announcing that I will not be continuing my position as president of MAGLab, nor will I be running for any other leadership positions for personal reasons.  I will still be around to advise anyone who fills the position as well as other officer positions where the incumbent is replaced.</p>
+<br>
+<p>If you cannot attend in person, please contact me or make yourself heard via any one of our communication streams and we will set up remote participation for you.</p>
+<br>
+<p>
+Regards,
+<br>
+Trenton Wilson
+<br>
+President <president@maglaboratory.org>
+<br>
+MAG Lab
+</p>
+
+</td>
+</tr>
+<tr>
+<td>
+<hr>
+<p style='font-size: normal; font-weight: bold'>
+Please email contact@maglaboratory.org if you have any questions.
+</p>
+<hr>
+<p style='font-size:small;'>
+A small note
+</p>
+<h2 style='border: 1px solid black;text-align:center;'>
+<a href='http://www.maglaboratory.org/donate' style='text-decoration:none;color:'>Click here to Donate</a>
+</h2>
+<h2 style='border: 1px solid black;text-align:center;'>
+<a href='http://www.maglaboratory.org/subscribe' style='text-decoration:none;color:'>Subscribe to (Weekly) Mailing List</a>
+</h2>
+<h2 style='border: 1px solid black; text-align:center;'>
+<a href='https://groups.google.com/forum/?fromgroups#!forum/maglaboratory' style='text-decoration:none;'>Google Groups/Forum</a>
+</h2>
+<h2 style='border: 1px solid black; text-align:center;'>
+<a href='http://webchat.freenode.net?channels=%23MAGLaboratory' style='text-decoration:none;'>IRC (freenode: #MAGLaboratory)</a>
+</h2>
+<h2 style='border: 1px solid black; text-align:center;'>
+<a href='https://www.facebook.com/MAGLaboratory/' style='text-decoration:none;'>Facebook</a>
+</h2>
+</td>
+</tr>
+
+</table>
+</div>
+</td>
+</tr>
+</table>
+<table style='clear: both !important; width:100%;'>
+<tr>
+<td></td>
+<div style='display: block; max-width: 600px;margin:0 auto;'>
+<table style='width: 100%;'>
+<tr>
+<td align='center'>
+</td>
+</tr>
+</table>
+</div>
+</tr>
+</table>
+</body>
+</html>
