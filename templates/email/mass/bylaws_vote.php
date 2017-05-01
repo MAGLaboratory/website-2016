76841e677b77be99f69d595306e93fb5bad35fb0
Kia Kroas <kiafaldorius@gmail.com>
2017-04-30 23:44:26 -0700

    bylaws vote email

---
+++ templates/email/mass/bylaws_vote.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta content='width=device-width' name='viewport'>
+<meta content='text/html; charset=UTF-8' http-equiv='Content-Type'>
+<title>MagLaboratory Email</title>
+</head>
+<body bgcolor='#f6f6f6' style='height:100%;width:100%;'>
+<table style='width:100%;padding:20px;'>
+<tr>
+<td></td>
+<td bgcolor='#fafafa' class='container' style='clear: both !important; display: block !important; max-width: 600px !important; margin: 0 auto; padding: 20px; border: 1px solid #efefef;'>
+<div style='display:block;max-width:600px;margin: 0 auto;'>
+<table bgcolor='#fafafa' style='width:100%;'>
+<tr>
+<td>
+<img alt='MAG Laboratory Logo' src='http://www.maglaboratory.org/images/email-logo.png'>
+</td>
+</tr>
+<tr>
+<td>
+<h1>Special Vote</h1>
+<br>
+<p>This is an important message from MAGLab.</p>
+<br>
+<p>This is an official ballot measure to adopt the new bylaws available here: https://groups.google.com/forum/?fromgroups#!topic/maglaboratory/-_zP7Nx3hpw</p>
+<p>
+The vote is open on
+<a href=''>Facebook Group</a>
+and
+<a href=''>Google Groups.</a>
+The May 2nd general meeting will be used to collect paper ballots. You may also reply to this email to cast your vote.
+</p>
+<br>
+<p>Please reply with one of the following: Approve or Deny. You may also abstain by not replying or "Abstain".</p>
+<br>
+<p>
+Thanks,
+<br>
+Hong Q.
+<br>
+Treasurer
+<br>
+MAG Lab
+</p>
+
+</td>
+</tr>
+<tr>
+<td>
+<hr>
+<p style='font-size: normal; font-weight: bold'>
+Please email contact@maglaboratory.org if you have any questions.
+</p>
+<hr>
+<p style='font-size:small;'>
+A small note
+</p>
+<h2 style='border: 1px solid black;text-align:center;'>
+<a href='http://www.maglaboratory.org/donate' style='text-decoration:none;color:'>Click here to Donate</a>
+</h2>
+<h2 style='border: 1px solid black;text-align:center;'>
+<a href='http://www.maglaboratory.org/subscribe' style='text-decoration:none;color:'>Subscribe to Mailing List</a>
+</h2>
+<h2 style='border: 1px solid black; text-align:center;'>
+<a href='https://groups.google.com/forum/?fromgroups#!forum/maglaboratory' style='text-decoration:none;'>Google Groups/Forum</a>
+</h2>
+<h2 style='border: 1px solid black; text-align:center;'>
+<a href='https://www.facebook.com/MAGLaboratory/' style='text-decoration:none;'>Facebook</a>
+</h2>
+</td>
+</tr>
+
+</table>
+</div>
+</td>
+</tr>
+</table>
+<table style='clear: both !important; width:100%;'>
+<tr>
+<td></td>
+<div style='display: block; max-width: 600px;margin:0 auto;'>
+<table style='width: 100%;'>
+<tr>
+<td align='center'>
+</td>
+</tr>
+</table>
+</div>
+</tr>
+</table>
+</body>
+</html>
