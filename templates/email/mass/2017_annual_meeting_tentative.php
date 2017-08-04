b33a024c5b163ea0e107cb5ec7478cb9a3230ffe
Kia Kroas <kiafaldorius@gmail.com>
2017-08-03 20:09:56 -0700

    add annual meeting message

---
+++ templates/email/mass/2017_annual_meeting_tentative.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta content='width=device-width' name='viewport'>
+<meta content='text/html; charset=UTF-8' http-equiv='Content-Type'>
+<title>MagLaboratory Email</title>
+</head>
+<body bgcolor='#f6f6f6' style='height:100%;width:100%;'>
+<table style='width:100%;padding:20px;'>
+<tr>
+<td></td>
+<td bgcolor='#fafafa' class='container' style='clear: both !important; display: block !important; max-width: 600px !important; margin: 0 auto; padding: 20px; border: 1px solid #efefef;'>
+<div style='display:block;max-width:600px;margin: 0 auto;'>
+<table bgcolor='#fafafa' style='width:100%;'>
+<tr>
+<td>
+<img alt='MAG Laboratory Logo' src='http://www.maglaboratory.org/images/email-logo.png'>
+</td>
+</tr>
+<tr>
+<td>
+<h1>2017 Annual Meeting</h1>
+<br>
+<p>This is an important message from MAGLab.</p>
+<br>
+<p>Hi everyone!</p>
+<br>
+<p>Summer is ending, which means it's that time of the year again...MAGLab's annual meeting is coming up.</p>
+<br>
+<p>It has been tentatively scheduled for Saturday, September 30, 2017 at 5pm. Based on previous annual meetings, it is expected to last 2 to 4 hours.</p>
+<br>
+<p>The annual meeting is very important for the organization. Attendance is highly encouraged (and necessary for quorum).</p>
+<br>
+<br>
+<p>Important topics of discussion:</p>
+<br>
+<p>
+<ul>
+<li>Voting of Executive Officers (President, Treasurer, Secretary)</li>
+<li>Voting of Board of Directors and other positions</li>
+<li>Update on our nonprofit filing with the IRS and CA FTB (state)</li>
+<li>Review of MAGLab, past, present, future and direction for the coming year</li>
+<li>Assessment of Bylaws and accompanying policies</li>
+<li>Strategies for activities, growth, etc</li>
+<li>Member concerns, comments, etc</li>
+<li>Incoming Board Members and Executive Officers will receive a booklet detailing duties and responsibilities</li>
+</ul>
+</p>
+<br>
+<p>We will have remote attendance options available. Likely Skype or Google Hangouts...maybe even conference call. Let us know if you would like to attend remotely.</p>
+<br>
+<p>Proxy attendance/voting can also be made available. For those who need it, please let us know in writing (email or text message will count) who your designated proxy shall be.</p>
+<br>
+<p>Please let us know if you will be available or not on September 30, 2017 and what time would be best for that day.</p>
+<br>
+<p>As of last month's membership count, we will need at least 12 members in attendance for quorum. Please check your schedules. The meeting can not take place without quorum.</p>
+<br>
+<p>Another notice will go out in a few weeks, after we get input and finalize the time. That second notice will be the official notice of meeting and will open up nominations for officers and Board of Directors.</p>
+<br>
+<p>
+Kind Regards,
+<br>
+Hong
+<br>
+Treasurer
+<br>
+MAG Lab
+</p>
+
+</td>
+</tr>
+<tr>
+<td>
+<hr>
+<p style='font-size: normal; font-weight: bold'>
+Please email contact@maglaboratory.org if you have any questions.
+</p>
+<hr>
+<p style='font-size:small;'>
+A small note
+</p>
+<h2 style='border: 1px solid black;text-align:center;'>
+<a href='http://www.maglaboratory.org/donate' style='text-decoration:none;color:'>Click here to Donate</a>
+</h2>
+<h2 style='border: 1px solid black;text-align:center;'>
+<a href='http://www.maglaboratory.org/subscribe' style='text-decoration:none;color:'>Subscribe to Mailing List</a>
+</h2>
+<h2 style='border: 1px solid black; text-align:center;'>
+<a href='https://groups.google.com/forum/?fromgroups#!forum/maglaboratory' style='text-decoration:none;'>Google Groups/Forum</a>
+</h2>
+<h2 style='border: 1px solid black; text-align:center;'>
+<a href='https://www.facebook.com/MAGLaboratory/' style='text-decoration:none;'>Facebook</a>
+</h2>
+</td>
+</tr>
+
+</table>
+</div>
+</td>
+</tr>
+</table>
+<table style='clear: both !important; width:100%;'>
+<tr>
+<td></td>
+<div style='display: block; max-width: 600px;margin:0 auto;'>
+<table style='width: 100%;'>
+<tr>
+<td align='center'>
+</td>
+</tr>
+</table>
+</div>
+</tr>
+</table>
+</body>
+</html>
